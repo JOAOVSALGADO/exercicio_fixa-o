c2baffd7ffd9305504ae42c17c1ef8ee050cbf64
Joao V. Salgado <joaovsalgado22@gmail.com>
2021-11-14 21:37:22 -0300

    Mudei a interação inserindo um botão(input) onde confirmo o horario do dia e dependendo que cada horario ele me da um resultado diferente.

--- js.js
+++ js.js
@@ -2,15 +2,17 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var msg2 = window.document.getElementById('msg2')
+    var num = window.document.getElementById('number')
+    var num2 = Number(num.value)
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 22;
-    msg.innerHTML = "Agora são " + hora + " Horas."
-    if (hora >= 0 && hora < 12 ){
+    
+    msg.innerHTML = "Agora são " + num2 + " horas."
+    if (num2 >= 0 && num2 < 12 ){
        
         img.src = 'MANHA2.png'
         msg2.innerHTML = "Bom dia!!!"
-    } else if (hora >= 12 && hora < 18){
+    } else if (num2 >= 12 && num2 < 18){
         img.src ='TARDE.png'
         msg2.innerHTML = "Boa tarde!!!"
     } else {
